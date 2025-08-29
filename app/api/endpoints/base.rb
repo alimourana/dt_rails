edfb18176fffe44af3068983dafcc212df9bc12e
alimourana <alimourana@gmail.com>
2025-08-29 14:18:17 -0400

    Add user endpoints to API base
    - Mounted additional user-related endpoints: Delete, Create, Get, and Update in the API base module to enhance user management functionalities.

--- app/api/endpoints/base.rb
+++ app/api/endpoints/base.rb
@@ -4,6 +4,11 @@ module Endpoints
     format :json
     prefix :api
 
+    # Users
+    mount Endpoints::V1::Users::Delete
+    mount Endpoints::V1::Users::Create
+    mount Endpoints::V1::Users::Get
     mount Endpoints::V1::Users::List
+    mount Endpoints::V1::Users::Update
   end
 end
