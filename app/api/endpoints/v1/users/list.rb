b3dfa0b6b0fb9417b4d512071ca8677dbd14cefb
alimourana <alimourana@gmail.com>
2025-08-29 14:18:54 -0400

    Enhance Users List endpoint documentation and response structure
    - Updated the Users List endpoint to include detailed descriptions for success and failure responses, improving API documentation.
    - Changed the response serialization to use the Entities::Users model for better clarity in user data representation.
    - Added a namespace setting for categorization within the API.

--- app/api/endpoints/v1/users/list.rb
+++ app/api/endpoints/v1/users/list.rb
@@ -3,10 +3,23 @@ module Endpoints
     module Users
       class List < Grape::API
         resource :users do
-          desc 'Get all users'
+          namespace_setting(:category, "users")
+
+          desc 'Get all users', {
+            summary: 'Get all users',
+            detail: 'Get all users',
+            success: [
+              { code: 200, model: Entities::Users }
+            ],
+            failure: [
+              { code: 400, message: 'Bad Request' },
+              { code: 401, message: 'Unauthorized' },
+            ]
+          }
+          # endpoint "getAllUsers"
           get do
             users = User.all
-            present users, with: Entities::User
+            present users, with: Entities::Users
           end
         end
       end
