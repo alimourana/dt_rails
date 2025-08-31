4747ae6ab970116175d347bb3b2e2f9f97bbedf0
alimourana <alimourana@gmail.com>
2025-08-31 18:28:00 -0400

    Refactor rails_helper and spec_helper for consistency and improved error handling
    - Standardized string syntax to single quotes in both rails_helper.rb and spec_helper.rb.
    - Updated error handling in rails_helper.rb to log errors instead of printing them, enhancing debugging capabilities.
    - Added a new module, ApiTestHelpers, in rails_helper.rb for JSON response parsing, improving test utility.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require "rspec/rails"
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -22,21 +22,23 @@ require "rspec/rails"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').each do |f|
+  require f
+end
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+  Rails.logger.error e.to_s.strip
+  raise e
 end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
-    Rails.root.join("spec/fixtures")
+    Rails.root.join('spec', 'fixtures', 'fixtures')
   ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
@@ -55,8 +57,8 @@ RSpec.configure do |config|
   # explicitly tag your specs with their type, e.g.:
   #
   #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
+  # ...
+  # end
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
@@ -66,4 +68,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+# Helper methods for testing API endpoints
+module ApiTestHelpers
+  def json_response
+    JSON.parse(response.body)
+  end
+end
+
+RSpec.configure do |config|
+  config.include ApiTestHelpers, type: :request
 end
