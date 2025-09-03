281226d9788cc9147663e23fdb7a0d23cea00b59
alimourana <alimourana@gmail.com>
2025-09-03 17:04:13 -0400

    Add Doorkeeper configuration for OAuth applications
    - Configured Doorkeeper to use a custom controller for managing OAuth applications.
    - This change enhances the routing setup for the application, allowing for better organization and handling of OAuth-related requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  use_doorkeeper do
+    controllers :applications => 'oauth/applications'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   mount ::Endpoints::Base => '/'
