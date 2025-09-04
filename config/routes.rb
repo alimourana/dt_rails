119e278976792878ab4ae015a7889cf3dc0900d4
alimourana <alimourana@gmail.com>
2025-09-04 17:10:25 -0400

    Integrate Devise for user authentication and remove legacy routes
    - Added Devise routes for user registrations and sessions, streamlining authentication management.
    - Removed legacy authentication routes to enhance clarity and maintainability of the routing configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  
   use_doorkeeper do
     controllers :applications => 'oauth/applications'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Authentication routes
-  get 'sign_in', to: 'auth/sessions#new', as: :new_auth_session
-  post 'sign_in', to: 'auth/sessions#create', as: :auth_session
-  delete 'sign_out', to: 'auth/sessions#destroy', as: :destroy_auth_session
-  get 'sign_up', to: 'auth/registrations#new', as: :new_auth_registration
-  post 'sign_up', to: 'auth/registrations#create', as: :auth_registration
 
   # OAuth application management routes
   resources :oauth_applications, path: 'oauth/applications'
