e50082619276069f4b98d1430a988f9fda562ddd
alimourana <alimourana@gmail.com>
2025-09-04 17:10:39 -0400

    Update database schema and add Devise fields for user authentication
    - Updated the schema version to reflect recent migrations.
    - Renamed the 'type' column to 'document_type' in the documents table for clarity.
    - Added necessary fields for Devise user authentication, including password recovery, trackable attributes, and confirmation features.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_03_122008) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_04_205526) do
   create_table "billing_rates", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "origin", null: false
     t.string "destination", null: false
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_03_122008) do
     t.string "title", null: false
     t.string "description"
     t.string "file_path"
-    t.string "type"
+    t.string "document_type"
     t.string "number"
     t.string "status"
     t.date "delivery_date"
@@ -167,7 +167,25 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_03_122008) do
     t.boolean "is_active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "delivery_notes", "citernes", column: "citernes_id"
