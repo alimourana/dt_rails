ec8b595e600b12dd840febdaf536a20f29748280
alimourana <alimourana@gmail.com>
2025-09-04 17:07:23 -0400

    Refactor ApplicationController to integrate Devise parameter sanitization
    - Removed legacy user authentication methods and replaced them with Devise parameter sanitization for sign-up and account update.
    - Streamlined the controller by eliminating unused methods and enhancing the configuration of permitted parameters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,36 +8,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  rescue ActiveRecord::RecordNotFound
-    session[:user_id] = nil
-    nil
-  end
-
-  def authenticate_user!
-    redirect_to sign_in_path unless current_user
-  end
-
-  def sign_in_path
-    new_auth_session_path
-  end
-
-  def stored_location
-    session[:stored_location]
-  end
-
-  def store_location
-    session[:stored_location] = request.fullpath
-  end
-
   def configure_permitted_parameters
-    # This method is here for compatibility but not used in this implementation
-  end
-
-  helper_method :current_user, :user_signed_in?
-
-  def user_signed_in?
-    current_user.present?
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :address_line, :city, :state, :country, :role, :is_active])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :address_line, :city, :state, :country, :role, :is_active])
   end
 end
